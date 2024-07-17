5e7da001e364080ef98385711b23848dc71e589b
Pavel Orlovsky <62477509+fendrarog@users.noreply.github.com>
2024-07-18 00:03:58 +0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import DrawerAppBar from '@/components/AppBar/AppBar'
+import { ServicePanel } from '@/components/ServicePanel/ServicePanel'
+
+
+function App() {
+
+  return (
+    <>
+      <DrawerAppBar />
+      <ServicePanel />
+    </>
+  )
+}
+
+export default App
